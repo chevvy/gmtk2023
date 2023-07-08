c1d386733ab6b5a3f71ed4cf39fea14e20111800
alix <lelievre.alix@gmail.com>
2023-07-08 00:04:11 -0400

    Adds basic main menu. "Start" load the scene at index 1 in the build settings. "Quit" exits the app or stops the play session in the editor.

---
+++ Assets/Sources/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Sources.UI.MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void OnStartButton()
+        {
+            Debug.Log("START");
+            SceneManager.LoadScene(1);
+        }
+        
+        public void OnQuitButton()
+        { 
+#if UNITY_STANDALONE
+            Application.Quit();
+#endif
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
