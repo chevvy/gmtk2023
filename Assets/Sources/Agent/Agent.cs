dae9ef33fb42bd3f98ff32a996155cb5d005dc6b
alix <lelievre.alix@gmail.com>
2023-07-07 22:54:26 -0400

    Working NavMesh agent mechanics & ennemy shoot

--- Assets/Sources/Agent/Agent.cs
+++ Assets/Sources/Agent/Agent.cs
@@ -1,38 +1,96 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 namespace Sources.Agent
 {
     public class Agent : MonoBehaviour
     {
-        [FormerlySerializedAs("Player")] public GameObject player;
+        public GameObject player;
+        private bool _playerSpotted;
+
+        public NavMeshAgent navMeshAgent;
+
+        public GameObject projectilePrefab;
+        public float projectileLifetimeInS = 10f;
+        public float attackRange = 20f;
+        public float attackCooldownInS = 1f;
+        private float _attackedLastAtInS;
 
         private void Awake()
         {
+            Debug.Assert(gameObject.CompareTag("Agent"));
+            
             player = GameObject.Find("Player");
-        }
-        
-        private bool IsPlayerInLos()
-        {
-            var source = transform.position;
-            var target = player.transform.position;
-            var direction = target - source;
-            var layer = player.layer;
-
-            RaycastHit hit;
-            if (Physics.Raycast(source, direction, out hit, layer))
-            {
-                Debug.Log("Ass");
-                return true;
-            }
-
-            return false;
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            navMeshAgent.stoppingDistance = attackRange - 5;
         }
         
         private void FixedUpdate()
         {
-            Debug.Log(IsPlayerInLos() ? "ASS" : "LOS");
-        }   
+            if (IsPlayerSpotted()) NavigateTowardsPlayer();
+            if (IsPlayerInRange() && IsPlayerInLos()) AttackPlayer();
+        }
+
+        private Vector3 GetDirectionTowardsPlayer()
+        {
+            var position = transform.position;
+            var target = player.transform.position;
+
+            return target - position;
+        }
+        
+        private bool IsPlayerInLos()
+        {
+            var collided = Physics.Raycast(transform.position, GetDirectionTowardsPlayer(), out var hit, Mathf.Infinity);
+
+            return collided && hit.collider.gameObject.CompareTag(player.tag);
+        }
+
+        private bool IsPlayerSpotted()
+        {
+            return _playerSpotted || IsPlayerInLos();
+        }
+
+        private bool IsPlayerInRange()
+        {
+            var distance = Vector3.Distance(transform.position, player.transform.position);
+
+            return distance <= attackRange;
+        }
+
+        private void NavigateTowardsPlayer()
+        {
+            var destination = player.transform.position;
+
+            navMeshAgent.SetDestination(destination);
+        }
+
+        private bool IsAttackOnCooldown()
+        {
+            return _attackedLastAtInS + attackCooldownInS > Time.time;
+        }
+
+        private void SetAttackOnCooldown()
+        {
+            _attackedLastAtInS = Time.time;
+        }
+
+        private void SendAttackProjectileTowardsPlayer()
+        {
+            var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            var rigidbody = projectile.GetComponent<Rigidbody>();
+            var force = Vector3.Normalize(GetDirectionTowardsPlayer()) * 10 + Vector3.Normalize(transform.up) * 5;
+            
+            rigidbody.AddRelativeForce(force, ForceMode.Impulse);
+            
+            Destroy(projectile, projectileLifetimeInS);
+        }
+
+        private void AttackPlayer()
+        {
+            if (IsAttackOnCooldown()) return;
+            SetAttackOnCooldown();
+            SendAttackProjectileTowardsPlayer();
+        }
     }
 }
