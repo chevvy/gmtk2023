76e30288f33f26ec23e201e9898834e915b1bb5c
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-07-08 12:33:30 -0400

    Fix player controller prefab

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -9,7 +9,7 @@ public class Door : MonoBehaviour
 {
    [FormerlySerializedAs("requiredKeyColors")] public KeyColor requiredKeyColor;
    public Animator animator;   
-   public GameObject Player;
+   [FormerlySerializedAs("Player")] public GameObject player;
    private static readonly int Open = Animator.StringToHash("Open");
 
    private void Awake()
@@ -18,11 +18,14 @@ public class Door : MonoBehaviour
       {
          Debug.LogError("Missing animator reference");
       }
+      
+      player = GameObject.Find("Player");
+      
    }
 
    private void OnTriggerEnter(Collider other)
    {
-      if (!other.gameObject.CompareTag(Player.tag)) return;
+      if (!other.gameObject.CompareTag(player.tag)) return;
       
       var inventory = other.gameObject.GetComponent<Inventory>();
       if (inventory.HasKey(requiredKeyColor))
