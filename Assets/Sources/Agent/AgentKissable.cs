147d8c8b650d3c015a7dec334598321e12f2f338
alix <lelievre.alix@gmail.com>
2023-07-09 00:22:42 -0400

    Adds flashing effect to agent hit with kiss. Easily copy-pastable. mercibonsoir

--- Assets/Sources/Agent/AgentKissable.cs
+++ Assets/Sources/Agent/AgentKissable.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Sources.Agent
 {
@@ -15,8 +16,29 @@ namespace Sources.Agent
         {
             agent.GetComponent<MeshRenderer>().material.color = Color.red;
             agent.GetComponent<Agent>().pacified = true;
+
+            StartCoroutine(BlinkAgent());
             
             Debug.Log("Agent was kissed <3");
+        }
+
+        IEnumerator BlinkAgent()
+        {
+            var renderer = agent.GetComponent<MeshRenderer>();
+            var stopTime = Time.time + 2f;
+            var initialColor = renderer.material.color;
+            var flashColor = Color.gray;
+
+            for (int i = 0; i < 5; i++)
+            {
+                renderer.material.color = flashColor;
+
+                yield return new WaitForSeconds(0.4f);
+
+                renderer.material.color = initialColor;
+
+                yield return new WaitForSeconds(0.4f);
+            }
         }
     }
 }
